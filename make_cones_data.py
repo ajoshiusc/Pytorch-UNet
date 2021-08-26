bcf89e93b63afaaff2d8be742ef35282eb4c2734
Anand A. Joshi <ajoshi@usc.edu>
2021-08-26 16:15:27 -0700

    update

--- make_cones_data.py
+++ make_cones_data.py
@@ -9,8 +9,8 @@ C = [0, 0]
 
 rad = 1.0
 
-data = np.zeros((num_samples,256,256),dtype=np.float16)
-masks = np.zeros((num_samples,256,256),dtype=np.uint8)
+data = np.zeros((num_samples, 256, 256), dtype=np.float16)
+masks = np.zeros((num_samples, 256, 256), dtype=np.uint8)
 
 xx = np.linspace(C[0] + rad * -128, C[0] + rad * 128, 256)
 yy = np.linspace(C[1] + rad * -128, C[1] + rad * 128, 256)
@@ -24,7 +24,7 @@ img = Image.fromarray(R).convert("L")
 img.save('my.png')
 img.show()
 
-img = Image.fromarray(255*M).convert("L")
+img = Image.fromarray(255 * M).convert("L")
 img.save('my_msk.png')
 img.show()
 
@@ -37,7 +37,6 @@ for i in tqdm(range(num_samples)):
 
     radmsk = 128 * random.random()
     M = np.uint8(R < radmsk)
-
     '''
     img = Image.fromarray(R).convert("L")
     img.save('my.png')
@@ -51,8 +50,6 @@ for i in tqdm(range(num_samples)):
     R1 = np.expand_dims(R, axis=0)
     M = np.expand_dims(M, axis=0)
 
-    data[i,] = R #= np.concatenate((data, R1))
-    masks[i,] = M # = np.concatenate((masks, M))
-
-
-np.savez('cone_data_sim.npz',data=data,masks=masks)
+    data[i, ] = R  #= np.concatenate((data, R1))
+    masks[i, ] = M  # = np.concatenate((masks, M))
+np.savez('cone_data_sim.npz', data=data, masks=masks)
