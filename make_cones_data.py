0c9cc2df9b4e8271de0cd52edb43ec9593fd8d88
Anand A. Joshi <ajoshi@usc.edu>
2021-08-26 14:55:09 -0700

    update

--- make_cones_data.py
+++ make_cones_data.py
@@ -3,7 +3,7 @@ from PIL import Image
 from tqdm import tqdm
 import random
 
-num_samples = 10000
+num_samples = 1000
 
 C = [0, 0]
 
@@ -18,13 +18,13 @@ yy = np.linspace(C[1] + rad * -128, C[1] + rad * 128, 256)
 X, Y = np.meshgrid(xx, yy)
 
 R = np.sqrt(X**2 + Y**2)
-M = 255 * np.uint8(R < 50)
+M = np.uint8(R < 50)
 
 img = Image.fromarray(R).convert("L")
 img.save('my.png')
 img.show()
 
-img = Image.fromarray(M).convert("L")
+img = Image.fromarray(255*M).convert("L")
 img.save('my_msk.png')
 img.show()
 
@@ -35,8 +35,8 @@ random.seed(0)
 
 for i in tqdm(range(num_samples)):
 
-    radmsk = 100 * random.random()
-    M = 255 * np.uint8(R < radmsk)
+    radmsk = 128 * random.random()
+    M = np.uint8(R < radmsk)
 
     '''
     img = Image.fromarray(R).convert("L")
