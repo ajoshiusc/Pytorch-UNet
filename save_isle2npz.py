41fe3f5870e2f66b1e6985140ad02b73cec0a762
Anand A. Joshi <ajoshi@usc.edu>
2021-09-01 10:56:27 -0700

    update

--- save_isle2npz.py
+++ save_isle2npz.py
@@ -7,18 +7,19 @@ import nilearn as nl
 from skimage.exposure import match_histograms
 from sklearn.feature_extraction.image import extract_patches_2d
 from tqdm import tqdm
+import cv2
 
 
 def read_data_test(study_dir,
-              ref_dir,
-              subids,
-              nsub,
-              psize,
-              npatch_perslice,
-              slicerange,
-              erode_sz=1,
-              lesioned=False,
-              dohisteq=False):
+                   ref_dir,
+                   subids,
+                   nsub,
+                   psize,
+                   npatch_perslice,
+                   slicerange,
+                   erode_sz=1,
+                   lesioned=False,
+                   dohisteq=False):
     # erode_sz: reads the mask and erodes it by given number of voxels
     #    dirlist = glob.glob(study_dir + '/TBI*')
     subno = 0
@@ -28,26 +29,21 @@ def read_data_test(study_dir,
         ref_imgs_set = True
         t1_file = os.path.join(ref_dir, 'T1mni.nii.gz')
         #t1_mask_file = os.path.join(study_dir, subj, 'T1mni.mask.nii.gz')
-        t2_file = os.path.join(ref_dir,'T2mni.nii.gz')
-        flair_file = os.path.join(ref_dir,'FLAIRmni.nii.gz')
-        
-
+        t2_file = os.path.join(ref_dir, 'T2mni.nii.gz')
+        flair_file = os.path.join(ref_dir, 'FLAIRmni.nii.gz')
 
         t1 = nl.image.load_img(t1_file).get_fdata()
         t2 = nl.image.load_img(t2_file).get_fdata()
         flair = nl.image.load_img(flair_file).get_fdata()
-        
 
+        p = np.max(t1)
+        t1 = np.float32(t1) / p
 
+        p = np.max(t2)
+        t2 = np.float32(t2) / p
 
-        p=np.max(t1)
-        t1=np.float32(t1) / p
-
-        p=np.max(t2)
-        t2=np.float32(t2) / p
-
-        p=np.max(flair)
-        flair=np.float32(flair) / p
+        p = np.max(flair)
+        flair = np.float32(flair) / p
 
         ref_imgs = np.stack((t1, t2, flair), axis=3)
 
@@ -60,7 +56,6 @@ def read_data_test(study_dir,
         #p = np.percentile(np.ravel(flair), 99)  #normalize to 95 percentile
         #flair = np.float32(flair) / p
 
-        
     for subj in subids:
 
         t1_file = os.path.join(study_dir, subj, 'T1mni.nii.gz')
@@ -69,8 +64,8 @@ def read_data_test(study_dir,
         flair_file = os.path.join(study_dir, subj, 'FLAIRmni.nii.gz')
         seg = os.path.join(study_dir, subj, 'SEGMENTATIONmni.nii.gz')
 
-        if not (os.path.isfile(t1_file)
-                and os.path.isfile(t2_file) and os.path.isfile(flair_file)):
+        if not (os.path.isfile(t1_file) and os.path.isfile(t2_file)
+                and os.path.isfile(flair_file)):
             continue
 
         if subno < nsub:
@@ -84,13 +79,11 @@ def read_data_test(study_dir,
         flair = nl.image.load_img(flair_file).get_fdata()
         segment = nl.image.load_img(seg).get_fdata()
         t1_msk = nl.image.load_img(t1_file).get_fdata()
-        t1_msk[t1_msk!=0]=1
+        t1_msk[t1_msk != 0] = 1
 
-        
         #t1_msk = binary_erosion(t1_msk, iterations=erode_sz)
 
         imgs = np.stack((t1, t2, flair), axis=3)
-
 
         if ref_imgs_set == False:
             ref_imgs = imgs
@@ -100,14 +93,16 @@ def read_data_test(study_dir,
             imgs = match_histograms(image=imgs,
                                     reference=ref_imgs,
                                     multichannel=True)
-        
-        t1_msk=np.float32(t1_msk)
-        t1_msk=np.reshape(t1_msk,(t1_msk.shape[0],t1_msk.shape[1],t1_msk.shape[2],1))
-        imgs=imgs*t1_msk
-        segment=np.reshape(segment,(segment.shape[0],segment.shape[1],segment.shape[2],1))
+
+        t1_msk = np.float32(t1_msk)
+        t1_msk = np.reshape(
+            t1_msk, (t1_msk.shape[0], t1_msk.shape[1], t1_msk.shape[2], 1))
+        imgs = imgs * t1_msk
+        segment = np.reshape(
+            segment, (segment.shape[0], segment.shape[1], segment.shape[2], 1))
         imgs = np.concatenate((imgs, segment), axis=3)
 
-        imgs=imgs[:, :,slicerange,:]
+        imgs = imgs[:, :, slicerange, :]
 
         # Generate random patches
         # preallocate
@@ -136,31 +131,47 @@ def read_data_test(study_dir,
     return patch_data  # npatch x width x height x channels
 
 
-
 data_dir = '/big_disk/ajoshi/ISLES2015/preproc/Training/'
-ref_dir='/big_disk/ajoshi/fitbir.old/preproc/maryland_rao_v1/TBI_INVNU820VND'
+ref_dir = '/big_disk/ajoshi/fitbir.old/preproc/maryland_rao_v1/TBI_INVNU820VND'
 #study_dir ='/big_disk/ajoshi/fitbir/preproc/maryland_rao_v1/'
 with open('/big_disk/ajoshi/ISLES2015/ISLES2015_Training_done.txt') as f:
     tbidoneIds = f.readlines()
 tbidoneIds = [l.strip('\n\r') for l in tbidoneIds]
 window_H = 182
 window_W = 218
+
 slicerange = np.arange(81, 101, dtype=int)
+#slicerange = np.arange(0, 182, dtype=int)
+
 data = read_data_test(study_dir=data_dir,
-                                ref_dir=ref_dir,
-                                subids=tbidoneIds,
-                                nsub=28,
-                                psize=[window_H, window_W],
-                                npatch_perslice=1,
-                                slicerange=slicerange,
-                                erode_sz=0,
-                                lesioned=False,
-                                dohisteq=True
-                                )
-#data=data[:,:,81:101]
+                      ref_dir=ref_dir,
+                      subids=tbidoneIds,
+                      nsub=28,
+                      psize=[window_H, window_W],
+                      npatch_perslice=1,
+                      slicerange=slicerange,
+                      erode_sz=0,
+                      lesioned=False,
+                      dohisteq=True)
+
 fig, ax = plt.subplots()
 im = ax.imshow(data[0, :, :, 0])
 plt.show()
-#np.savez('data_24_ISEL_histeq.npz', data=data)
+
+data2 = np.zeros((data.shape[0],128,128,4))
+for i in tqdm(range(data.shape[0])):
+    data2[i,:,:,:3] = cv2.resize(data[i,:,:,:3],dsize=(128, 128),interpolation=cv2.INTER_CUBIC)
+    data2[i,:,:,3] = cv2.resize(data[i,:,:,3],dsize=(128, 128),interpolation=cv2.INTER_NEAREST)
+
+data = data2
+
+
+#np.savez('/big_disk/ajoshi/ISLES2015/ISEL_28sub_slices_81_101_histeq.npz', data=data)
 np.savez('/big_disk/ajoshi/ISLES2015/ISEL_28sub_slices_81_101_histeq.npz', data=data)
-#np.savez('/big_disk/ajoshi/ISLES2015/ISEL_28sub_slices_0_182_histeq.npz', data=data)
+
+S = np.sum(data[:,:,:,3],axis=(1,2))
+
+data = data[S>=1,:,:,:]
+
+#np.savez('/big_disk/ajoshi/ISLES2015/ISEL_28sub_slices_81_101_histeq_nonzeroslices.npz', data=data)
+np.savez('/big_disk/ajoshi/ISLES2015/ISEL_28sub_slices_81_101_histeq_nonzeroslices.npz', data=data)
